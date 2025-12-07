947a1a202215a64edbfc458b55d569c8fa022766
anoborame <zacharymccrth@gmail.com>
2025-12-07 15:59:54 -0500

    day 7 part 2

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -44,6 +44,30 @@ impl OrderedPushSet {
     }
 }
 
+struct OrderedPushSetPart2 {
+    values: Vec<(usize, u64)>,
+}
+
+impl OrderedPushSetPart2 {
+    fn push(self: &mut Self, next_value: (usize, u64)) {
+        // slightly modified versions that keeps track of the number of paths that lead to this
+        // index
+        match self.values.last_mut() {
+            Some((last_value_index, current_number_of_paths)) => {
+                if next_value.0 == *last_value_index {
+                    *current_number_of_paths += next_value.1;
+                } else {
+                    self.values.push(next_value)
+                }
+            }
+            None => self.values.push(next_value),
+        }
+    }
+
+    fn iter(&self) -> std::slice::Iter<'_, (usize, u64)> {
+        self.values.iter()
+    }
+}
 fn part_1(input: &str) -> u32 {
     let mut input_iter = input.lines();
     // initialize tachyons based on first line
@@ -73,12 +97,42 @@ fn part_1(input: &str) -> u32 {
     output
 }
 
+fn part_2(input: &str) -> u64 {
+    let mut input_iter = input.lines();
+    // initialize tachyons based on first line
+    let first_line = input_iter.next().unwrap();
+    let source_index = first_line.find(SOURCE).unwrap();
+
+    let mut current_tachyons_index = OrderedPushSetPart2 {
+        values: vec![(source_index, 1)],
+    };
+
+    for line in input_iter {
+        let mut next_tachyon_line = OrderedPushSetPart2 { values: Vec::new() };
+        for (tachyon_index, number_of_paths) in current_tachyons_index.iter() {
+            if line.as_bytes()[*tachyon_index] == SPLITTER {
+                next_tachyon_line.push((*tachyon_index - 1, *number_of_paths));
+                next_tachyon_line.push((*tachyon_index + 1, *number_of_paths));
+            } else {
+                next_tachyon_line.push((*tachyon_index, *number_of_paths));
+            }
+        }
+
+        current_tachyons_index = next_tachyon_line;
+    }
+
+    current_tachyons_index.values.iter().map(|v| v.1).sum()
+}
 fn main() {
     let input = read_input();
 
     let part_1_answer = part_1(&input);
 
     println!("Part 1 Solution: {}", part_1_answer);
+
+    let part_2_answer = part_2(&input);
+
+    println!("Part 2 Solution: {}", part_2_answer);
 }
 
 #[cfg(test)]
@@ -87,7 +141,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_1() {
         let input = String::from(
             ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............\n",
         );
@@ -95,5 +149,16 @@ mod tests {
         let part_1_answer = part_1(&input);
 
         assert_eq!(part_1_answer, 21);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = String::from(
+            ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............\n",
+        );
+
+        let part_2_answer = part_2(&input);
+
+        assert_eq!(part_2_answer, 40);
     }
 }
